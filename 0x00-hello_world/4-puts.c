f47924dd670f3aeca74264ba071a4915b816b2dd
“Ayoola <olasunkanmiayoola83@gmail.com>
2022-10-13 16:27:12 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,17 @@
-#!/bin/bash
-int puts("Programming is like building a multilingual puzzle")
+#include <stdio.h>
+/**
+ *
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 (success) if not, exit 
+ *
+ *    */
+
+int main(void)
+{
+      
+      	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}	
