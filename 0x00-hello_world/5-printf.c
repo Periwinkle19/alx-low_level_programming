1be4f7a2ca4b07109547112eeefd10dfed70834b
“Ayoola <olasunkanmiayoola83@gmail.com>
2022-10-13 17:43:06 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - print the phrase "with proper grammar, but the outcome is a piece of art"
+ * main - print with proper grammar, but the outcome is a piece of art
  * Return : 0 (success)
  */
 int main(void)
 {
-<<<<<<< HEAD
 	printf("with proper grammar, but the outcome is a piece of art");
-=======
-	printf("with proper grammar, but the outcome is a piece of art")
->>>>>>> 3a329b2d0e4e62d1484f686cdb5ac5e3aa2e7ba7
-	return 0;
+	return (0);
 }
