72f924db25fb087aa9a8942e3a4d03801baf9fd2
“Ayoola <olasunkanmiayoola83@gmail.com>
2022-10-17 21:47:50 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ *
+ * Return: (0) success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 100; n++)
+	{
+		putchar(n + '0');
+		if (n != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
