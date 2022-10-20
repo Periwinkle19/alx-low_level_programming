11d030282f0b026e58d724e1cbb6aa99c03ee233
“Ayoola <olasunkanmiayoola83@gmail.com>
2022-10-19 22:05:28 -0700

    master

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	int n;
+	for (n = 'a'; n <= 'z'; n++)
+
+	{
+		_putchar(n);
+	}
+	putchar ('\n');
+}
