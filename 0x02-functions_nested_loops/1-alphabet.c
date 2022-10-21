327cb706d29ce383cb46ce50c1df1ff71808b8b3
“Ayoola <olasunkanmiayoola83@gmail.com>
2022-10-21 07:03:57 -0700

    master

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,5 +11,5 @@ void print_alphabet(void)
 	{
 		_putchar(n);
 	}
-	putchar ('\n');
+	_putchar ('\n');
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet,in lowercase,
+ * followed by a new line.
+ * Return: (0) success
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int n;
+
+	n = 0;
+
+	while (n <= 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
+
