2965e5aba75dc14a7ff8e70887613f315966979b
“Ayoola <olasunkanmiayoola83@gmail.com>
2022-10-21 07:35:35 -0700

    lower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * islower - check for lowercase character
+ * @c - character to lookout 
+ * Return: 1 if c is lowercase and 0 otherwise
+ */
+int _islower(int c)
+{
+	return ( c >= 'a' && c <= 'z');
+}
