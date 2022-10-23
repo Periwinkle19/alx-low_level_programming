036890f724c6fe8ab6e47ca18ce7d2a292c11667
“Ayoola <olasunkanmiayoola83@gmail.com>
2022-10-23 09:38:40 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,23 +11,18 @@
  */
 int main(void)
 {
-	int i;
-
-	for (int i = 1; i <= 100; i++)
+	
+	for (int a = 1; a <= 100; a++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
-
+		if (a % 3 == 0 && a % 5 != 0)
 			printf("Fizz");
-		else if (i % 5 == 0 && i % 3 != 0)
-
+		else if (a % 5 == 0 && a % 3 != 0)
 			printf("Buzz");
-		else if (i % 3 == 0 && i % 5 == 0)
-
+		else if (a % 3 == 0 && a % 5 == 0)
 			printf("FizzBuzz");
 		else
-
-			printf(" %d", i);
-		if (i < 100)
+			printf(" %d", a);
+		if (a < 100)
 			printf(" ");
 	}
 	printf("\n");
