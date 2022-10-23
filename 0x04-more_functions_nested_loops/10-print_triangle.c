3bc3d41975e6b1b2c4fae153b1e4e7335a9ccff7
“Ayoola <olasunkanmiayoola83@gmail.com>
2022-10-23 10:28:44 -0700

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,20 +6,20 @@
  */
 void print_triangle(int size)
 {
-	if (size <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i, j;
+	int i, j;
 
-		for (i = 0; i <= size; i++)
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
 		{
-			for (j = 0; j <= size; j++)
+			if (j <= (size - i))
 			{
+				_putchar(' ');
+			} else
 				_putchar('#');
-			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
