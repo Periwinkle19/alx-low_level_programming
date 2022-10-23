89cca0d0642c08c49fd97105e96f1c184a089c1c
“Ayoola <olasunkanmiayoola83@gmail.com>
2022-10-23 08:24:14 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,15 +20,10 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if (c > a && c == b)
+	else if (c > a && b < c)
 	{
 		largest = c;
 	}
-	else if (c == a && c == b)
-	{
-		largest = c;
-	}
-
 	return (largest);
 }
 
